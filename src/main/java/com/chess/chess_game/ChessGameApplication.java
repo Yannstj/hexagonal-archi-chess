ebdd3e3133b12cd53704246794e54e88addd13ec
Yannstj <yannick.saintjust@gmail.com>
2025-12-11 01:13:39 +0100

    launch app without DB config

--- src/main/java/com/chess/chess_game/ChessGameApplication.java
+++ src/main/java/com/chess/chess_game/ChessGameApplication.java
@@ -2,8 +2,16 @@ package com.chess.chess_game;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
+import org.springframework.boot.jdbc.autoconfigure.DataSourceTransactionManagerAutoConfiguration;
 
-@SpringBootApplication
+//@SpringBootApplication
+@SpringBootApplication(exclude = {
+    DataSourceAutoConfiguration.class,
+    DataSourceTransactionManagerAutoConfiguration.class,
+    HibernateJpaAutoConfiguration.class
+})
 public class ChessGameApplication {
 
 	public static void main(String[] args) {
